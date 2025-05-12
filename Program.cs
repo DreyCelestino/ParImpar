3c841ee723c4459deafaab262d6d9f377370fab3
DreyCelestino <andreycelestino05@gmail.com>
2025-05-12 09:09:12 -0300

    v1

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Clear();
+
+string input = "null";
+double output;
+
+Console.WriteLine("Escreva um número para saber se é par ou impar!");
+input = Console.ReadLine()!;
+
+while(!(int.TryParse(input, out int teste))){
+    Console.WriteLine("Não é número! Tente novamente.");
+    input = Console.ReadLine()!;
+}
+
+output = Convert.ToInt32(input)%2;
+
+if(output == 0){
+    Console.Write(input);
+    Console.WriteLine(" é par!");
+} 
+
+else{
+    Console.Write(input);
+    Console.WriteLine(" é impar!");
+}
